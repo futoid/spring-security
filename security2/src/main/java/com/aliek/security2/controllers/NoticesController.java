098d2de05b93ce449b43dbc7a3fdefdc439d35aa
futoid <aliekmandal24@gmail.com>
2025-03-02 09:11:15 +0530

    new: Override default SecurityFilterChain -> provides formloginand basic auth

---
+++ security2/src/main/java/com/aliek/security2/controllers/NoticesController.java
@@ -0,0 +1,12 @@
+package com.aliek.security2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+    @GetMapping("/notices")
+    public String getNotices(){
+        return "Notice Page";
+    }
+}
